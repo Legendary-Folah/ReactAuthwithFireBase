9906bdde04dbf170e9ef8c737c1b664228f4c44e
unknown <folarinfadeyi@gmail.com>
2023-02-14 15:24:40 -0800

    first commit here

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <form className='form'>
+            <h3>Login</h3>
+            <div className='form-row'>
+            <label className='label-form'>
+                Name
+            </label>
+            <input 
+                type='text' 
+                className='input-form' 
+                id='name'
+                value={name}
+            />
+            </div>
+            <div className='form-row'>
+            <label className='label-form'>
+                Email
+            </label>
+            <input 
+                type='email' 
+                className='input-form' 
+                id='email'
+                value={email}
+            />
+            </div>
+        </form>
+        <button type='submit'>Login</button>
     </div>
   );
 }
