9f121472599f79d613920436989ff1c2fab3c325
unknown <folarinfadeyi@gmail.com>
2023-02-18 15:48:53 -0800

    Making use of bootstrap. It is a lot easier

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -4,7 +4,11 @@ import { Form, Button, Card } from 'react-bootstrap';
 function SignUp() {
   return (
     <>
-      <Card></Card>
+      <Card>
+        <Card.Body>
+            <h2 className='text-center mb-4'>Sign Up</h2>
+        </Card.Body>
+      </Card>
       <div className='w-100 text-center mt-2'>
         Already have an acount? Log In here
       </div>
