4df28eace9c73682ed10dc77b75a5dbfcc6b4885
unknown <folarinfadeyi@gmail.com>
2023-02-18 16:36:48 -0800

    Making use of bootstrap. It is a lot easier

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import SignUp from './Components/SignUp';
+import Login from './Components/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<SignUp />} />
+            <Route path='/login' element={<Login />} />
         </Routes>
     </BrowserRouter>
   )
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,50 +1,50 @@
-// import React from "react"
+import React from "react"
 
-// const Login = () => {
+const Login = () => {
 
-//     const handleSubmit = e => {
-//         e.preventDefault();
-//     }
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
 
-//     return (
-//         <>
-//     <div className='App'>
-//         <form className='form'>
-//             <h5>Login</h5>
-//             <div className='form-row'>
-//             <label 
-//                 htmlFor='name' 
-//                 className='form-label'>
-//                 Name
-//             </label>
-//             <input 
-//                 type='text' 
-//                 className='form-input' 
-//                 id='name'
-//                 // value={name}
-//             />
-//             </div>
-//             <div className='form-row'>
-//             <label 
-//                 htmlFor='email'
-//                 className='form-label'>
-//                 Email
-//             </label>
-//             <input 
-//                 type='email' 
-//                 className='form-input' 
-//                 id='email'
-//                 // value={email}
-//             />
-//             </div>
-//         </form>
-//         <button 
-//             type='submit' 
-//             className='btn btn-block'
-//             onSubmit={handleSubmit}>
-//         Login
-//         </button>
-//     </div>
-//     </>
-//     )
-// }
+    return (
+        <>
+    <div className='App'>
+        <form className='form'>
+            <h5>Login</h5>
+            <div className='form-row'>
+            <label 
+                htmlFor='name' 
+                className='form-label'>
+                Name
+            </label>
+            <input 
+                type='text' 
+                className='form-input' 
+                id='name'
+                // value={name}
+            />
+            </div>
+            <div className='form-row'>
+            <label 
+                htmlFor='email'
+                className='form-label'>
+                Email
+            </label>
+            <input 
+                type='email' 
+                className='form-input' 
+                id='email'
+                // value={email}
+            />
+            </div>
+        </form>
+        <button 
+            type='submit' 
+            className='btn btn-block'
+            onSubmit={handleSubmit}>
+        Login
+        </button>
+    </div>
+    </>
+    )
+}
