70e48caf4f32e430282b9f7436e763bc2fd4b3c2
unknown <folarinfadeyi@gmail.com>
2023-02-18 16:05:03 -0800

    Making use of bootstrap. It is a lot easier

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -7,6 +7,7 @@ function SignUp() {
       <Card>
         <Card.Body>
             <h2 className='text-center mb-4'>Sign Up</h2>
+            <Form>
             <Form.Group id='email'>
                 <Form.Label>Email</Form.Label>
                 <Form.Control type='email' ref={emailRef} required />
@@ -15,10 +16,11 @@ function SignUp() {
                 <Form.Label>Password</Form.Label>
                 <Form.Control type='password' ref={passwordRef} required />
             </Form.Group>
-            <Form.Group id='confirm-password'>
+            <Form.Group id='password-confirm'>
                 <Form.Label>Confirm Password</Form.Label>
                 <Form.Control type='password' ref={confirmPasswordRef} required />
             </Form.Group>
+            </Form>
         </Card.Body>
       </Card>
       <div className='w-100 text-center mt-2'>
