839f59708df5901e8096381f8c461c959d920024
unknown <folarinfadeyi@gmail.com>
2023-02-18 16:54:23 -0800

    Making use of bootstrap. It is a lot easier

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Form, Button, Card } from "react-bootstrap";
 
 const Login = () => {
@@ -6,6 +6,8 @@ const Login = () => {
     const handleSubmit = e => {
         e.preventDefault();
     }
+
+    const emailRef = useRef();
 
     return (
         <>
@@ -16,6 +18,10 @@ const Login = () => {
                         <Form.Group id="email">
                             <Form.Label>Email</Form.Label>
                             <Form.Control type="email" ref={emailRef} required />
+                        </Form.Group>
+                        <Form.Group id="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" ref={passwordRef} required />
                         </Form.Group>
                     </Form>
                 </Card.Body>
