ff0547c7ce25c1eac055ecc9e22737e6e37350ec
unknown <folarinfadeyi@gmail.com>
2023-02-18 16:46:34 -0800

    Making use of bootstrap. It is a lot easier

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -8,45 +8,9 @@ const Login = () => {
 
     return (
         <>
-    <div className='App'>
-        <form className='form'>
-            <h5>Login</h5>
-            <div className='form-row'>
-            <label 
-                htmlFor='name' 
-                className='form-label'>
-                Name
-            </label>
-            <input 
-                type='text' 
-                className='form-input' 
-                id='name'
-                // value={name}
-            />
-            </div>
-            <div className='form-row'>
-            <label 
-                htmlFor='email'
-                className='form-label'>
-                Email
-            </label>
-            <input 
-                type='email' 
-                className='form-input' 
-                id='email'
-                // value={email}
-            />
-            </div>
-        </form>
-        <button 
-            type='submit' 
-            className='btn btn-block'
-            onSubmit={handleSubmit}>
-        Login
-        </button>
-    </div>
-    </>
-    )
-}
+            
+        </>
+    );
+};
 
 export default Login;
