f9809887f8301104c56f7edc11b83a5217ca255f
unknown <folarinfadeyi@gmail.com>
2023-02-20 09:25:59 -0800

    Making use of bootstrap. It is a lot easier

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <Routes>
             <Route path='/' element={<SignUp />} />
             <Route path='/login' element={<Login />} />
+            <Route path='*' element={<Error />} />
         </Routes>
     </BrowserRouter>
   )
