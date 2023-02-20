3cda563eaea9a958aa6df547f593d458d9e5c4f0
unknown <folarinfadeyi@gmail.com>
2023-02-20 11:01:25 -0800

    more changes and so on

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -10,6 +10,6 @@ const app = firebase.initializeApp({
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
 
-// export const auth = app.auth()
-export const auth = getAuth(firebaseApp);
+export const auth = app.auth()
+// export const auth = getAuth(firebaseApp);
 export default app;
