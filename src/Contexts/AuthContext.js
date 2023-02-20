3d66dd247f03884549fc278283afe8b9b39a4419
unknown <folarinfadeyi@gmail.com>
2023-02-20 10:06:21 -0800

    more changes and so on

--- src/Contexts/AuthContext.js
+++ src/Contexts/AuthContext.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AuthContext = React.createContext();
 
-function AuthProvider({ children }) {
+export function AuthProvider({ children }) {
   return (
     <AuthContext.Provider>
       {children}
