62757e5145ac7d45817547048ce7b30993652505
unknown <folarinfadeyi@gmail.com>
2023-02-20 09:49:09 -0800

    Making use of bootstrap. It is a lot easier

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import SignUp from './Components/SignUp';
 import Login from './Components/Login';
 import Error from './Components/Error';
-
+import { Container } from 'react-bootstrap';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
