8e4a9c58276443508846cfadd06e279f5e059355
unknown <folarinfadeyi@gmail.com>
2023-02-20 15:09:31 -0800

    more changes and so on

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const auth = getAuth(app);
 
-export default app;
+export default {app, auth};
 
