5c78fac7e4dd13677e861e49ccd06abb1a0e4844
unknown <folarinfadeyi@gmail.com>
2023-02-20 15:23:33 -0800

    more changes and so on

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,7 +1,7 @@
-import firebase from 'firebase/app';
 import { initializeApp } from "firebase/app";
 import 'firebase/auth';
 import { getAuth } from "firebase/compat/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
